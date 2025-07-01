5c361bb10bac2d8e9f35093747c9f797bc61bd05
jeonjeongbin <01jjb@naver.com>
2025-07-02 08:08:57 +0900

    실습 3일 차

---
+++ 00_init-proj2-vite/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    초기 셋팅
+    </>
+  )
+}
+
+export default App
---
+++ 01_init-proj-vite-test2/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+    초기 셋팅
+    </>
+  )
+}
+
+export default App
